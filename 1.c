5041b63041d9366e3382e6c18bba4c8391af03e6
hrszanini <hrszanini@gmail.com>
2022-12-03 23:11:59 -0300

    Correção do enunciado

--- 1.c
+++ 1.c
@@ -1,7 +1,7 @@
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 // Find the sum of all the multiples of 3 or 5 below 1000.
 
-// Se nós listarmos todos os números naturais abaixo de  10 que são mútiplos de 3 ou 5, nós obtemos 3, 5, 6 e 9. A soma desses múltiplos é 13.
+// Se nós listarmos todos os números naturais abaixo de  10 que são mútiplos de 3 ou 5, nós obtemos 3, 5, 6 e 9. A soma desses múltiplos é 23.
 // Encontre a soma de todos os múltiplos de 3 ou 5 abaixo de 1000.
 
 #include <stdio.h>
