c4b3c0ce98848c5a0e2bc2ff56d92f212b5b0a95
hrszanini <hrszanini@gmail.com>
2022-12-03 21:21:12 -0300

    Adicionado problema 1

--- 1.c
+++ 1.c
@@ -1,8 +1,23 @@
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Find the sum of all the multiples of 3 or 5 below 1000.
 #include <stdio.h>
+#define FINAL 10
+
+int isMultipleOf3And5(int number){
+    if(number % 3 == 0 || number % 5 == 0)
+        return 1;
+    return 0;
+}
 
 int main(){
-    
+    int sum = 0;
 
+    for(int i = 3; i < 1000; i++){
+        if(isMultipleOf3And5(i))
+            sum += i;
+    }
+
+    printf("%d\n", sum);
 
     return 0;
 }
--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Project Euler
 
 Soluções implementadas a fim de solucionar os problemas da plataforma [Project Euler](https://projecteuler.net/), tentando seguir sempre a regra de execução em menos de 1 minuto.
+
+- [x] Problema 1
+- [ ] Problema 2
+- [ ] Problema 3
+- [ ] Problema 4
+- [ ] Problema 5
+- [ ] Problema 6
+- [ ] Problema 7
+- [ ] Problema 8
+- [ ] Problema 9
+- [ ] Problema 10
